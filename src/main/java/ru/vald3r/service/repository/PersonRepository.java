43635948399699c7be5201825bd350c8c1e13a0f
Попов Владислав Васильевич <VPopov7@alfabank.ru>
2022-03-14 21:53:07 +0300

    add some

--- src/main/java/ru/vald3r/service/repository/PersonRepository.java
+++ src/main/java/ru/vald3r/service/repository/PersonRepository.java
@@ -4,9 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.vald3r.service.model.Person;
 
-import java.util.Optional;
-
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
-    Optional<Person> findByEmail(String email);
 }
